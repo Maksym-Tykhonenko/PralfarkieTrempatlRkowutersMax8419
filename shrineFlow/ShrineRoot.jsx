86b040abe78eba87e5ea3ecc3303f121abb07536
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2026-05-28 13:38:23 +0300

    fix otl

--- shrineFlow/ShrineRoot.jsx
+++ shrineFlow/ShrineRoot.jsx
@@ -75,7 +75,7 @@ export const ShrineRoot = () => {
 
   const ONESIGNAL_KEY = `56ccca99-6399-4982-9b1e-fb06e153f92d`;
 
-  const TARGET_DATA = new Date(2026, 4, 2, 8, 8, 0);
+  const TARGET_DATA = new Date(2026, 4, 30, 8, 8, 0);
 
 useEffect(() => {
   const fetchData = async () => {
